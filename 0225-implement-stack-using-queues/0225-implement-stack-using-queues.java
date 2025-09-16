1cf00080896cf675c0628075dea729429954da02
thejasree25 <g.thejasree15@gmail.com>
2025-09-16 11:36:24 +0530

    Time: 0 ms (100%), Space: 41.4 MB (65.74%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,44 +1,35 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class MyStack {
-    Queue<Integer> q1 = new LinkedList<>();
-    Queue<Integer> q2 = new LinkedList<>();
-
+    Queue<Integer> q;
     public MyStack() {
-        // Constructor
+        q=new LinkedList<>();
     }
-
+    
     public void push(int x) {
-        // Move all elements from q1 to q2
-        while (!q1.isEmpty()) {
-            q2.offer(q1.poll());
-        }
-
-        // Add the new element to q1
-        q1.offer(x);
-
-        // Move everything back from q2 to q1
-        while (!q2.isEmpty()) {
-            q1.offer(q2.poll());
+        q.add(x);
+        for(int i=0;i<q.size()-1;i++){
+            int temp=q.poll();
+            q.add(temp);
         }
     }
-
+    
     public int pop() {
-        if (!q1.isEmpty()) {
-            return q1.poll(); // Remove and return the front (top of stack)
-        }
-        return -1; // or throw exception
+        return q.poll();
     }
-
+    
     public int top() {
-        if (!q1.isEmpty()) {
-            return q1.peek(); // Peek front element
-        }
-        return -1; // or throw exception
+        return q.peek();
     }
-
+    
     public boolean empty() {
-        return q1.isEmpty(); // q1 holds all elements
+        return q.isEmpty();
     }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
